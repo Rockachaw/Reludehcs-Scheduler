3dbe20b2bac9cac2c0b00c225b4aad68541934ec
RishiBhojwani <90673785+RishiBhojwani@users.noreply.github.com>
2021-11-14 03:13:32 -0600

    Made website better

--- pages/index.js
+++ pages/index.js
@@ -2,74 +2,111 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { VStack } from '@chakra-ui/layout'
 import { Box } from '@chakra-ui/layout'
-{var name }
-
-class SimpleForm extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            form_name: "",
-            price: ""
-        }
-    }
-
-
-    render()
-    {
-        return (
-            <form>
-                <div className="form-group">
-                    <input value={this.state.form_name} onChange={(e) => { this.setState({form_name: e.target.value}) }}/>
-                </div>
-            </form>
-        );
-    }
-}
-
-class ParentOfForm extends React.Component
-{
-    render()
-    {
-        return (
-            <SimpleForm howDoIsendToInputField="garbage" />
+import { StyleSheet, Text, View } from "react"
+import { Input } from "@chakra-ui/react"
+//import React, { Component } from 'react';
+import {
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	FormHelperText,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	NumberIncrementStepper,
+	NumberDecrementStepper,
+  } from "@chakra-ui/react"
+  import React from "react";
+  //import { Input } from "reactstrap";
+  import { Image } from '@chakra-ui/image'
+import { urlObjectKeys } from 'next/dist/shared/lib/utils'
 
 
-        )
 
-    }
+function HomePage() {			
 
-}
-function HomePage() {
-		SimpleForm
 	return(
 		<>
-		<Head>
-  			<title>Sample</title>
-  			<link rel="icon" href="/favicon.ico" />
+		<Box style={{backgroundImage: "url(https://ospa.utdallas.edu/ospa/files/utdallas-15718735-1100.jpg)",backgroundColor: "darkorange",height: "380px"}}>
+			<Image text="center" boxSize="100px" src="https://i1.wp.com/utdesports.com/wp-content/uploads/2018/12/cropped-UTDmono_OL_flame.png?fit=576%2C576&ssl=1"/>
+		</Box>
+		<div style={{backgroundColor: "DarkOrange",height: "700px"}}>
+		<Head style>
+  			<title>Reldehcs-Scheduler</title>
+  			<link rel="icon" href="https://i1.wp.com/utdesports.com/wp-content/uploads/2018/12/cropped-UTDmono_OL_flame.png?fit=576%2C576&ssl=1" />
 		</Head>
 		
-		<h1>First post</h1>
-		<h2>
-        		<Link href="https://www.google.com/">
-          		<a>Google the answer</a>
-        		</Link>
-      		</h2>
-		<div>Sample text</div>
+		<h1 style={{fontSize: "50px",textAlign: "center",color: "green",backgroundColor: "DarkOrange"}}>Reldehcs-Scheduler</h1>
+		{/*
+		<FormControl id="amount">
+  			<FormLabel>Number of Classes</FormLabel>
+  			<NumberInput max={10} min={1}>
+    		<NumberInputField />
+			<NumberInputStepper>
+      		<NumberIncrementStepper />
+      		<NumberDecrementStepper />
+    		</NumberInputStepper>
+  		</NumberInput>
+		</FormControl>
 
+
+		
+		{/*
 		<VStack spacing="24px">
-  			<Box w="40px" h="40px" bg="yellow.200">
-    			1
+  			<Box w="800px" h="60px" bg="yellow.200">
+			  <Input placeholder="Input Class" />
   			</Box>
-  			<Box w="40px" h="40px" bg="tomato">
-    			2
+  			<Box w="800px" h="60px" bg="yellow.200">
+			  <Input placeholder2="Input Class" />
   			</Box>
-  			<Box w="40px" h="40px" bg="pink.100">
-    			3
+  			<Box w="800px" h="60px" bg="yellow.200">
+			  <Input placeholder3="Input Class" />
   			</Box>
 		</VStack>
+		*/}
+		<div >
+		<VStack>
+		<FormControl id1="class1">
+  		<FormLabel style={{color: "green", textAlign: "center"}}>Enter Class 1</FormLabel>
+  		<Input type="class" style={{maxWidth: "800px", backgroundColor: "white", position: "absolute",right: "220px"}}/>
+		</FormControl>
+		<a style={{color: "DarkOrange"}}>______________________________________________________________________________________</a>
+		<FormControl id="class2">
+  		<FormLabel style={{color: "green", textAlign: "center"}}>Enter Class 2</FormLabel>
+  		<Input type="class" style={{maxWidth: "800px", backgroundColor: "white", position: "absolute",right: "220px"}}/>
+		</FormControl>
+		<a style={{color: "DarkOrange"}}>______________________________________________________________________________________</a>
+		<FormControl id="class3">
+  		<FormLabel style={{color: "green", textAlign: "center"}}>Enter Class 3</FormLabel>
+  		<Input type="class" style={{maxWidth: "800px", backgroundColor: "white", position: "absolute",right: "220px"}} />
+		</FormControl>
+		<a style={{color: "DarkOrange"}}>______________________________________________________________________________________</a>
+		<FormControl id="class4">
+  		<FormLabel style={{color: "green", textAlign: "center"}}>Enter Class 4</FormLabel>
+  		<Input type="class" style={{maxWidth: "800px", backgroundColor: "white", position: "absolute",right: "220px"}}/>
+		</FormControl>
+		<a style={{color: "DarkOrange"}}>______________________________________________________________________________________</a>
+		<FormControl id="class5">
+  		<FormLabel style={{color: "green", textAlign: "center"}}>Enter Class 5</FormLabel>
+  		<Input type="class" style={{maxWidth: "800px", backgroundColor: "white", position: "absolute",right: "220px"}}/>
+		</FormControl>
+		<a style={{color: "DarkOrange"}}>______________________________________________________________________________________</a>
+		<FormControl id="class6">
+  		<FormLabel style={{color: "green", textAlign: "center"}}>Enter Class 6</FormLabel>
+  		<Input type="class" style={{maxWidth: "800px", backgroundColor: "white", position: "absolute",right: "220px"}}/>
+		</FormControl>
+		<a style={{color: "DarkOrange"}}>______________________________________________________________________________________</a>
+		</VStack>
+		<a style={{color: "DarkOrange"}}>______________________________________________________________________________________</a>
+		<Box style={{position: "absolute",right:"610px"}}w="60px" h="40px" bg="green" borderRadius="1g">
+				<Link href="https://www.google.com/">
+          		
+				<a style= {{textAlign: "center"}}>Submit</a>
+        		</Link>
+		</Box>
+		</div>
 		
+		</div>
 		</>
 	)
 }
