7d46e8e419a7206d0e08da45adfbbdd1a1ea80c2
Houston Holman <hdh200000@utdallas.edu>
2021-11-14 03:15:56 -0600

    added api stuff

--- pages/hello.js
+++ pages/hello.js
@@ -2,22 +2,65 @@ import React from 'react';
 import { Heading, HStack, VStack } from '@chakra-ui/react';
 
 const Home = () => {
-  const [name, setName] = React.useState('');
 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('/api/hello');
-      const data = await res.json();
-      setName(data.name);
-    };
+    const [data, setData] = React.useState('[{}]');
+    var classData = [];
 
-    fetchData();
+    React.useEffect(() => {
+        const fetchData = async () => {
+            var myHeaders = new Headers();
+            myHeaders.append("Authorization", "dd1h55UQUb8x5nQIPW2iJ1ABaIDx9iv7");
+        
+            var requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                redirect: 'follow'
+            };
+        
+            fetch("https://floating-sea-33671.herokuapp.com/https://api.utdnebula.com/v1/sections/search?course_number=1337&course_prefix=cs", requestOptions)
+                .then(response => response.text())
+                .then(data => setData(data))
+                .catch(error => console.log('error', error));
+
+
+            /*fetch("https://floating-sea-33671.herokuapp.com/https://api.utdnebula.com/v1/sections/search?course_number=3390&course_prefix=ecs", requestOptions)
+                .then(response => response.text())
+                .then(data => classData.push(data))
+                .catch(error => console.log('error', error));*/
+
+
+        }
+        const test = (data) => {
+          console.log(data);
+          //console.log(JSON.parse(data)[0].instructors)
+        }
+
+        /*const updateData = (data) => {
+          classData.push(data)
+          setData(data)
+        }*/
+
+        /*const chooseBestProf = (classData) => {
+            //var json = JSON.parse(classData);
+            //var bestProf = {};
+            //bestProf.instructor = json[0].instructors
+            console.log(classData[0]);
+            console.log(classData[1]);
+            setData(classData[0]);
+
+        }*/
+    
+        fetchData();
   }, []);
 
   return (
     <VStack>
-      <Heading>{name}</Heading>
-      <Heading>After the name</Heading>
+      <Heading>{JSON.parse(data)[0].instructors}</Heading>
+      <Heading>Rate My Professor Score: 5</Heading>
+      <Heading>{JSON.parse(data)[0].section_name}</Heading>
+      <Heading>{JSON.parse(data)[0].days}</Heading>
+      <Heading>{JSON.parse(data)[0].times}</Heading>
+      <Heading>{JSON.parse(data)[0].location}</Heading>
     </VStack>
   );
 };
